19a8747ea6d83399f9966b174936d0e9672563da
deck16 <deck16@users.noreply.github.com>
2019-03-01 16:35:53 +0700

    Rect drawing routines added

--- Graphics.cpp
+++ Graphics.cpp
@@ -22,3 +22,16 @@ void Graphics::PutPixel(int x, int y, sf::Color c)
     pixel.setPosition(sf::Vector2f(x,y));
     wnd.draw(pixel);
 }
+
+void Graphics::DrawRect(int x0, int y0, int x1, int y1, sf::Color c)
+{
+    if (x0 > x1)
+        std::swap(x0, x1);
+    if (y0 > y1)
+        std::swap(y0, y1);
+
+    sf::RectangleShape rect(sf::Vector2f(x1-x0, y1-y0));
+    rect.setFillColor(c);
+    rect.setPosition(sf::Vector2f(x0,y0));
+    wnd.draw(rect);
+}
--- Graphics.hpp
+++ Graphics.hpp
@@ -7,6 +7,11 @@ class Graphics
 public:
     Graphics(sf::RenderWindow& wnd);
     void PutPixel(int x, int y, sf::Color c = sf::Color::White);
+    void DrawRect(int x0, int y0, int x1, int y1, sf::Color c);
+    void DrawRectDim(int x0, int y0, int width, int height, sf::Color c = sf::Color::White)
+    {
+        DrawRect(x0, y0, x0+width, y0+height, c);
+    }
     void BeginFrame();
     void EndFrame();
 public:
