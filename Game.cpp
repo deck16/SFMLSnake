2ee8b3d5626a006c79cc4edb635c319317eb271f
deck16 <deck16@users.noreply.github.com>
2019-03-01 16:36:26 +0700

    Basic Game functionality set up

--- Game.cpp
+++ Game.cpp
@@ -1,9 +1,14 @@
 #include "Game.hpp"
+#include <iostream>
 
 Game::Game(sf::RenderWindow& wnd)
     :
     wnd(wnd),
-    gfx(wnd)
+    gfx(wnd),
+    rng(std::random_device()()),
+    brd(gfx),
+    snek(brd, {1,1}),
+    goal(brd)
 {}
 
 void Game::Go()
@@ -16,8 +21,50 @@ void Game::Go()
 
 void Game::UpdateModel()
 {
+    const float dt = ft.Mark();
+    waitTime += dt;
+
+    if (kbd.isKeyPressed(sf::Keyboard::W))
+    {
+        dir = {0, -1};
+    }
+    if (kbd.isKeyPressed(sf::Keyboard::D))
+    {
+        dir = {1, 0};
+    }
+    if (kbd.isKeyPressed(sf::Keyboard::A))
+    {
+        dir = {-1, 0};
+    }
+    if (kbd.isKeyPressed(sf::Keyboard::S))
+    {
+        dir = {0, 1};
+    }
+
+    if ( waitTime > snekMovePeriod)
+    {
+        if  (kbd.isKeyPressed(sf::Keyboard::Space))
+        {
+            snek.Grow();
+        }
+
+        snek.MoveBy(dir);
+        waitTime = 0;
+
+        if (snek.IsOutsideBoard() || snek.IsInTile())
+        {
+            gameOver = true;
+        }
+
+        snek.CheckAndEatGoal(rng, goal);
+    }
 }
 
 void Game::ComposeFrame()
 {
+    if (!gameOver)
+    {
+        goal.Draw();
+        snek.Draw();
+    }
 }
--- Game.hpp
+++ Game.hpp
@@ -1,5 +1,10 @@
 #pragma once
 #include "Graphics.hpp"
+#include "Board.hpp"
+#include "Snake.hpp"
+#include "FrameTimer.hpp"
+#include "Goal.hpp"
+#include <random>
 
 class Game
 {
@@ -11,5 +16,17 @@ private:
     void ComposeFrame();
 private:
     sf::RenderWindow& wnd;
+    sf::Keyboard kbd;
     Graphics gfx;
+    FrameTimer ft;
+    std::mt19937 rng;
+    Board brd;
+    Snake snek;
+    Goal goal;
+    Location dir = {1,0};
+
+    float waitTime = 0.0f;
+    float snekMovePeriod = 0.2f;
+
+    bool gameOver = false;
 };
