e816403d83c6ff39edd65eda7608f41980540865
deck16 <deck16@users.noreply.github.com>
2019-03-06 23:19:25 +0700

    State interface implemented, which will be inherited by any game state class

---
+++ include/IState.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+class IState
+{
+public:
+    virtual void Init() = 0;
+    virtual void HandleInput() = 0;
+    virtual void Update(float dt) = 0;
+    virtual void Draw() = 0;
+
+    virtual void Pause() {}
+    virtual void Resume() {}
+};
